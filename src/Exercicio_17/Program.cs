6dae639d15a826be6566f8ccb75b913d8ce09df0
RaulORios <raul.orios89@outlook.com>
2021-08-04 18:43:13 -0300

    Program.cs adicionado

---
+++ src/Exercicio_17/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Exercicio_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //ToDo - Alterar variavel Int para decimal.
+
+            Console.WriteLine("****************************************");
+            Console.WriteLine("*********     Exercicio 17     *********");
+            Console.WriteLine("****************************************");
+
+            Console.WriteLine("");
+
+            int Nota1;
+            int Nota2;
+            int media;
+
+            Console.Write("Digite a 1º Nota: ");
+            Nota1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Digite a 2º Nota: ");
+            Nota2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("");
+
+            media = (Nota1 + Nota2) / 2;
+
+            if (media >= 6)
+            {
+                Console.WriteLine($"A media do aluno é {media} - Aluno APROVADO.");
+            }
+            else
+            {
+                Console.WriteLine($"A media do aluno é {media} - Aluno REPROVADO.");
+            }
+            
+            Console.ReadKey();
+
+        }
+    }
+}
